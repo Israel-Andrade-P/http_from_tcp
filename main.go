8948670cfc05582fba25bfdbebaa77b61017feca
zel <isral888andrade@gmail.com>
2025-10-26 09:37:04 -0300

    create getLinesChannel now spawning a goroutine to read the file and passing lines through a channel. Simulating HTTP Streams.

--- main.go
+++ main.go
@@ -13,25 +13,36 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error opening file %v", err)
 	}
-	defer file.Close()
+	for line := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	linesChan := make(chan string)
 
 	buffer := make([]byte, 8)
 	currentLine := ""
 
-	for {
-		n, err := file.Read(buffer)
-		if err != nil {
-			if err == io.EOF {
-				break
+	go func() {
+		defer f.Close()
+		for {
+			n, err := f.Read(buffer)
+			if err != nil {
+				if err == io.EOF {
+					close(linesChan)
+					break
+				}
+				log.Fatalf("Error reading file %v", err)
 			}
-			log.Fatalf("Error reading file %v", err)
+			parts := strings.Split(string(buffer[:n]), "\n")
+			currentLine += strings.Join(parts[:1], " ")
+			if len(parts) > 1 {
+				linesChan <- currentLine
+				currentLine = ""
+				currentLine += string(parts[1])
+			}
 		}
-		parts := strings.Split(string(buffer[:n]), "\n")
-		currentLine += strings.Join(parts[:1], " ")
-		if len(parts) > 1 {
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = ""
-			currentLine += string(parts[1])
-		}
-	}
+	}()
+	return linesChan
 }
