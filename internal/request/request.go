79522c0c6357615d3142a8ef9b31cb4cc683340b
zel <154630339+Israel-Andrade-P@users.noreply.github.com>
2025-12-03 11:14:44 -0300

    read request in chunks now, but only request line for now

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,13 +1,14 @@
 package request
 
 import (
+	"bytes"
 	"fmt"
 	"io"
-	"strings"
 )
 
 type Request struct {
 	RequestLine RequestLine
+	state       parserState
 }
 
 type RequestLine struct {
@@ -16,59 +17,106 @@ type RequestLine struct {
 	Method        string
 }
 
-func RequestFromReader(reader io.Reader) (*Request, error) {
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
+type parserState string
 
-	req, err := parseRequestLine(string(data))
-	if err != nil {
-		return nil, err
-	}
+const (
+	StateInit  parserState = "init"
+	StateDone  parserState = "done"
+	StateError parserState = "error"
+)
 
-	return req, nil
+var (
+	ErrorMalformedRequestLine  = fmt.Errorf("malformed request-line")
+	ErrorUnsuportedHttpVersion = fmt.Errorf("unsuported http version")
+	ErrorRequestInErrorState   = fmt.Errorf("request in error state")
+	separator                  = []byte("\r\n")
+)
+
+func (r *Request) parse(data []byte) (int, error) {
+	read := 0
+
+outer:
+	for {
+		switch r.state {
+		case StateError:
+			return 0, ErrorRequestInErrorState
+		case StateInit:
+			rl, n, err := parseRequestLine(data[read:])
+			if err != nil {
+				r.state = StateError
+				return 0, err
+			}
+
+			if n == 0 {
+				break outer
+			}
+
+			r.RequestLine = *rl
+			read += n
+			r.state = StateDone
+		case StateDone:
+			break outer
+		}
+	}
+	return read, nil
 }
 
-func parseRequestLine(req string) (*Request, error) {
-	methods := []string{"GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS", "TRACE"}
-	parts := strings.Split(req, "\r\n")
-	reqLine := strings.Split(parts[0], " ")
-	if len(reqLine) != 3 {
-		return nil, fmt.Errorf("invalid request line")
-	}
-	if !isAllUpperAlpha(reqLine[0]) || !contains(methods, reqLine[0]) {
-		return nil, fmt.Errorf("invalid method")
-	}
-	httpVersion := strings.Split(reqLine[2], "/")
-	if httpVersion[1] != "1.1" {
-		return nil, fmt.Errorf("invalid http version")
-	}
-	request := &Request{
-		RequestLine: RequestLine{
-			HttpVersion:   httpVersion[1],
-			RequestTarget: reqLine[1],
-			Method:        reqLine[0],
-		},
+func (r *Request) isDone() bool {
+	return r.state == StateDone || r.state == StateError
+}
+
+func newRequest() *Request {
+	return &Request{state: StateInit}
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	request := newRequest()
+
+	buf := make([]byte, 1024)
+	bufLen := 0
+	for !request.isDone() {
+		n, err := reader.Read(buf[bufLen:])
+		if err != nil {
+			return nil, err
+		}
+
+		bufLen += n
+
+		readN, err := request.parse(buf[:bufLen])
+		if err != nil {
+			return nil, err
+		}
+
+		copy(buf, buf[readN:bufLen])
+		bufLen -= readN
 	}
 
 	return request, nil
 }
 
-func isAllUpperAlpha(str string) bool {
-	for _, l := range str {
-		if l < 'A' || l > 'Z' {
-			return false
-		}
+func parseRequestLine(b []byte) (*RequestLine, int, error) {
+	idx := bytes.Index(b, separator)
+	if idx == -1 {
+		return nil, 0, nil
 	}
-	return true
-}
 
-func contains(methods []string, target string) bool {
-	for _, method := range methods {
-		if method == target {
-			return true
-		}
+	requestLine := b[:idx]
+	read := idx + len(separator)
+	parts := bytes.Split(requestLine, []byte(" "))
+	if len(parts) != 3 {
+		return nil, 0, ErrorMalformedRequestLine
 	}
-	return false
+
+	httpParts := bytes.Split(parts[2], []byte("/"))
+	if len(httpParts) != 2 || string(httpParts[0]) != "HTTP" || string(httpParts[1]) != "1.1" {
+		return nil, 0, ErrorMalformedRequestLine
+	}
+
+	rl := &RequestLine{
+		Method:        string(parts[1]),
+		RequestTarget: string(parts[1]),
+		HttpVersion:   string(httpParts[1]),
+	}
+
+	return rl, read, nil
 }
