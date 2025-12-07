f76fea52a6e50a39ae9b2492bb92e2b673e2897c
zel <154630339+Israel-Andrade-P@users.noreply.github.com>
2025-12-07 14:48:16 -0300

    add token validation for field line key and now we accept multiple header values for the same key

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -9,19 +9,37 @@ import (
 
 func TestParse(t *testing.T) {
 	// Test: Valid single header
-	headers := NewHeaders()
-	data := []byte("Host: localhost:42069\r\n\r\n")
-	n, done, err := headers.Parse(data)
+	h := NewHeaders()
+	data := []byte("Host: localhost:42069\r\nFooFoo:     barbar      \r\n\r\n")
+	n, done, err := h.Parse(data)
 	require.NoError(t, err)
-	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
-	assert.Equal(t, 23, n)
-	assert.False(t, done)
+	require.NotNil(t, h.headers)
+	assert.Equal(t, "localhost:42069", h.Get("Host"))
+	assert.Equal(t, "barbar", h.Get("FooFoo"))
+	assert.Equal(t, "", h.Get("MissingKey"))
+	assert.Equal(t, 51, n)
+	assert.True(t, done)
+
+	// Test: Valid single header
+	h = NewHeaders()
+	data = []byte("Content-Type: application/json\r\nContent-Type: text\r\n\r\n")
+	n, done, err = h.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, h.headers)
+	assert.Equal(t, "application/json, text", h.Get("CONTENT-TYPE"))
 
 	// Test: Invalid spacing header
-	headers = NewHeaders()
+	h = NewHeaders()
 	data = []byte("       Host : localhost:42069       \r\n\r\n")
-	n, done, err = headers.Parse(data)
+	n, done, err = h.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid token header key
+	h = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = h.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
